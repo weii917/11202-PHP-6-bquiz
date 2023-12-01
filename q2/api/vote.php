cacda7a271d2ed579e6f136a8a88dde715106a15
wei <lin951016@gmail.com>
2023-12-01 16:31:08 +0800

    註解

--- q2/api/vote.php
+++ q2/api/vote.php
@@ -1,8 +1,9 @@
 <?php include_once "../db.php";
-// $_POST['opt']得到的是選項id，透過他來找到這opt那一筆
+// $_POST['opt']得到的是選項id，透過他來找到這一筆選項
+// 從這個選項的count使count+1
 $opt=$Que->find($_POST['opt']);
 $opt['count']=$opt['count']+1;
-
+// 從這個選項的subject_id去撈出這個選項的題目的那一筆要讓count也加1
 $subject=$Que->find($opt['subject_id']);
 $subject['count']=$subject['count']+1;
 
