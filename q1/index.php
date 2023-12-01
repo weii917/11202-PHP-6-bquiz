2e7f26ec102ae1595e838f31d49469e4febfb417
wei <lin951016@gmail.com>
2023-12-01 09:27:06 +0800

    完成題組一更新圖片，建立題組二介面

--- q1/index.php
+++ q1/index.php
@@ -51,7 +51,7 @@ include_once "db.php";
                     <td><input type="text" name="text[]" id="" value="<?=$row['text'];?>" style="width:90%"></td>
                     <td><input type="radio" name="sh" id="" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>></td>
                     <td><input type="checkbox" name="del[]" id="" value="<?=$row['id']?>"></td>
-                    <td><input class="btn btn-info" type="button" value="更新圖片"></td>
+                    <td><input class='btn btn-primary' type="button" value="更新圖片" onclick="op('#cover','#cvr','upload_title.php?id=<?=$row['id'];?>')"></td>
                     <input type="hidden" name="id[]" value="<?=$row['id']?>">
                 </tr>
                 <?php
